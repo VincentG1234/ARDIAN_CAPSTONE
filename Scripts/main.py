d7515586772e210574714d343abfe54616d0606d
Vincent Gimenes <147169146+VincentG1234@users.noreply.github.com>
2025-02-24 16:14:12 +0100

    Add files via upload

---
+++ Scripts/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from Scripts.language_model.language_model import pipeline_model
+from Scripts.filter_user.ask_user import ask_all, ask_company, ask_keywords
+
+
+def searchmodel_main(df, model=None, tokenizer=None, top_n=10 ) -> pd.DataFrame:
+    
+    # Filtrage des données selon les préférences de l'utilisateur
+    df = ask_all(df)
+
+    # Selection de l'entreprise
+    index = ask_company(df)
+
+    print(index)
+    # Récupérer la description de l'entreprise choisie
+
+    if ask_keywords() is not None:
+        add_to_description_keywords = "KEY WORDS; ask_keywords()."
+    else:
+        add_to_description_keywords = ""
+
+    df.loc[index, "BUSINESS_DESCRIPTION"] = add_to_description_keywords + df.loc[index, "BUSINESS_DESCRIPTION"]
+
+    # Ajouter des mots-clés pour guider la recherche
+    # chosen_description = add_keywords_to_description(chosen_description)
+
+    # Appliquer le modèle BERT avec la nouvelle description enrichie
+    pipeline_model(df, index, model=model, tokenizer=tokenizer, top_n=top_n)
+
+
+
+
